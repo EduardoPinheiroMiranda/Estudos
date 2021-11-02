369cd5de6e05bdf790ca0d16dd14e0d46c35eae4
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2021-11-02 17:16:36 -0300

    Parte de login e cadastro
    A parte de login está quase finalizada.

--- P.I_Apresentação_de_infografico/PÁGINA_01_loguin/loguin.js
+++ P.I_Apresentação_de_infografico/PÁGINA_01_loguin/loguin.js
@@ -1,6 +1,11 @@
-let usuario = document.getElementById("usuario")
-let senha = document.getElementeById("senha")
+let usuario = document.getElementById('usuario')
+let senha = document.getElementeById('senha')
+let alerta = document.getElementById('alerta')
 
-function logar(){
-    
+function confirmar(){
+    if(senha.value == "1234"){
+        alert("certo")
+    }else{
+        alert("errado")
+    }
 }
